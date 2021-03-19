86052666dd5f81d0d0d04c0aaf186824b64eb851
Filippo Rossi <pippo.reds@gmail.com>
2021-03-19 21:48:13 +0100

    chore: update fish

--- dot_config/private_fish/executable_config.fish
+++ dot_config/private_fish/executable_config.fish
@@ -10,10 +10,6 @@ set PATH $HOME/.cargo/bin $PATH
 #
 set PATH $HOME/.cabal/bin $HOME/.ghcup/bin $PATH
 
-# pyenv
-set -Ux PYENV_ROOT $HOME/.pyenv
-set -Ux fish_user_paths $PYENV_ROOT/bin $fish_user_paths
-
 # pyenv init
 if command -v pyenv 1>/dev/null 2>&1
   status --is-interactive; and pyenv init - | source
@@ -22,7 +18,7 @@ end
 
 # ssh agent
 if test -z (pgrep ssh-agent)
-  eval (ssh-agent -c)
+  eval (ssh-agent -c) > /dev/null
   set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
   set -Ux SSH_AGENT_PID $SSH_AGENT_PID
   set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
