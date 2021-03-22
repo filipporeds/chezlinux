70b0109f67ae0844286063c732c6b6f0a0208653
Filippo Rossi <pippo.reds@gmail.com>
2021-03-22 09:52:50 +0100

    chore: update

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -13,8 +13,22 @@ if command -v pyenv 1>/dev/null 2>&1
   status --is-interactive; and pyenv virtualenv-init - | source
 end
 
+# XDG Directories
+set -gx XDG_CONFIG_HOME $HOME/.config
+set -gx XDG_CACHE_HOME $HOME/.cache
+set -gx XDG_DATA_HOME $HOME/.local/share
+
 # Adjust .files in home
 # source: << https://wiki.archlinux.org/index.php/XDG_Base_Directory >>
-set -gx CONDARC $XDG_CONFIG_HOME/conda/condarc
-set -gx IPYTHONDIR $XDG_CONFIG_HOME/jupyter
-set -gx JUPYTER_CONFIG_DIR $XDG_CONFIG_HOME/jupyter
+
+# - python
+set -gx CONDARC $XDG_CONFIG_HOME/python/conda/condarc
+set -gx IPYTHONDIR $XDG_CONFIG_HOME/python/jupyter
+set -gx JUPYTER_CONFIG_DIR $XDG_CONFIG_HOME/python/jupyter
+set -gx PYTHONHISTFILE $XDG_DATA_HOME/python_repl_history
+set -gx PYLINTHOME $XDG_CACHE_HOME/python
+
+set -gx NODE_REPL_HISTORY $XDG_DATA_HOME/node_repl_history
+set -gx NPM_CONFIG_USERCONFIG $XDG_CONFIG_HOME/npm/npmrc
+alias yarn="yarn --use-yarnrc $XDG_CONFIG_HOME/yarn/config"
+
---
+++ run_once_install-packages.fish
@@ -0,0 +1,5 @@
+#!/usr/bin/env fish
+
+# Manage node versions
+fisher install jorgebucaran/nvm.fish
+
